2c7208f3dee1712019c6fc0d6b4b7680b3dd4618
quan <jiahao.quan@me.com>
2021-06-14 18:36:04 -0400

    try catch

--- src/main/java/com/quan/quan/controller/HomeController.java
+++ src/main/java/com/quan/quan/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.quan.quan.service.UserService;
 import com.quan.quan.utili.JWTUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,17 +51,17 @@ public class HomeController {
 
     @PostMapping("/authenticate")
     public String authenticate(@RequestBody AuthRequest authRequest) throws Exception{
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                authRequest.getUsername(),
-                authRequest.getPassword()
-                )
-        );
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authRequest.getUsername(),
+                            authRequest.getPassword()
+                    )
+            );
+        }catch (BadCredentialsException e){
+            throw new Exception("incorrect username or password",e);
+        }
          UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
-
-
-
-
 
          String token = jwtUtility.generateToken(userDetails);
         return token;
